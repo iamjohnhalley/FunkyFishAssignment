1e4ec1fb073bf7097afa2071192b50fa6e6cc644
John Halley <johnhalley1994@gmail.com>
2017-05-04 10:18:50 +0100

    updates

---
+++ FunkyFish/MainMenuScene.swift
@@ -0,0 +1,18 @@
+//
+//  MainMenuScene.swift
+//  FunkyFish
+//
+//  Created by John on 04/05/2017.
+//  Copyright © 2017 John. All rights reserved.
+//
+
+import SpriteKit
+
+class MainMenuScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+        //john
+    }
+
+} //class
